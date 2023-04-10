6802ff579b54c5af1e304f831d99fe57af34e49c
Enock <enock.mokua@student.moringaschool.com>
2023-04-10 19:11:40 +0300

    set up users creation successfully

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
-                       sessions: "api/v1/sessions",
-                       registrations: "api/v1/registrations",
+                       sessions: "sessions",
+                       registrations: "registrations",
                      }
   # Defines the root path route ("/")
   # root "articles#index"
