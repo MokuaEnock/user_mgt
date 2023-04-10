f34f0c6df9e43a2245d4290d8fb67ee0903120a4
Enock <enock.mokua@student.moringaschool.com>
2023-04-10 18:51:02 +0300

    set up sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+  before_action :configure_sign_in_params, only: [:create]
+
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    sign_in(resource_name, resource)
+    render json: reosource
+  end
+
+  protected
+
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+  end
 end
