2712721935d6b98d996ddcef0e0e401b89965d22
Enock <enock.mokua@student.moringaschool.com>
2023-04-10 19:02:57 +0300

    set up registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,20 @@
 class RegistrationsController < ApplicationController
+  before_action :configure_sign_up_params, only: [:create]
+
+  def create
+    build_resource(sign_up_params)
+    resource.save
+    if resource.persisted?
+      sign_up(resource_name, resource)
+      render json: resource
+    else
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
+  end
 end
